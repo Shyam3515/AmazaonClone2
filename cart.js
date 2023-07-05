e55fa81368c1377b2e9781f12dcbaf82dd15ede5
Yadla Syam Sundar <115862918+Shyam3515@users.noreply.github.com>
2023-07-05 12:33:00 +0530

    Add files via upload

--- cart.js
+++ cart.js
@@ -1,10 +1,41 @@
 // const cart = [];
 let cart;
+//local storage
+function saveData() {
+    localStorage.setItem('product', JSON.stringify(cart));
+    console.log("local: ", localStorage.getItem('product'))
+};
 let savedData = JSON.parse(localStorage.getItem('product'));
 if(Array.isArray(savedData)){
     cart = savedData;
 }
 else{
     cart = [];
+}
+
+// Cart Update
+let cartQuantity = 0;
+cartUpdate();
+function cartUpdate() {
+    //Get the cart quantity and put it on the page
+    cart.forEach((item) => {
+        cartQuantity++;
+    });
+}  
+
+let orders = cart;
+
+//storing track order
+let track;
+function trackData() {
+    localStorage.setItem('track', JSON.stringify(track));
+    console.log("local: ", localStorage.getItem('track'))
+};
+let trackedData = JSON.parse(localStorage.getItem('track'));
+if(Array.isArray(trackedData)){
+    track = trackedData;
+}
+else{
+    track = [];
 }
 
